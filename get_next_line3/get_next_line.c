5aa01a7cb49623e7f1d6912856e651f437109088
Valeriia Krasnianska <vvkrasnianska@gmail.com>
2024-11-01 14:57:03 +0100

    ok version

---
+++ get_next_line3/get_next_line.c
@@ -0,0 +1,161 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvalerii <kvalerii@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 13:26:12 by kvalerii          #+#    #+#             */
+/*   Updated: 2024/11/01 14:56:18 by kvalerii         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+char	*ft_strncpy(char *dst, const char *src, size_t size)
+{
+	size_t i;
+
+	i = 0;
+	while (src[i] != '\0' && i < size)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	while (i < size)
+	{
+		dst[i] = '\0';
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strndup(char *src, size_t size)
+{
+	char *res;
+
+	res = malloc(size + 1);
+	if (!res)
+		return (NULL);
+	ft_strncpy(res, src, size);
+	res[size] = '\0';
+	return (res);
+}
+
+char	*ft_strjoin(char *stash, char *buffer, 
+		size_t size, size_t bytes_read)
+{
+	char	*res;
+
+	res = malloc((size + bytes_read) + 1);
+	if (!res)
+		return (NULL);
+	ft_strncpy(res, stash, size);
+	ft_strncpy(res + size, buffer, bytes_read);
+	res[size + bytes_read] = '\0';
+	free(stash);
+	return (res);
+}
+
+size_t	ft_find_index_in_range(char *s, char c, size_t start, size_t end)
+{
+	while(start <= end)
+	{
+		if (s[start] == c)
+			break;
+		start++;
+	}
+	return (start);
+}
+
+char	*ft_create_new_stash(char *old_stash, size_t new_line, size_t total_size)
+{
+	char	*new_stash;
+
+	new_stash = ft_strndup(old_stash + new_line, total_size - new_line);
+	if (!new_stash)
+		return (NULL);
+	free(old_stash);
+	return (new_stash);
+}
+
+char *get_next_line(int fd)
+{
+	static char	*stash;
+	ssize_t		bytes_read;
+	size_t		total_size;
+	char		buffer[BUFFER_SIZE];
+	char		*line;
+	size_t		new_line_index;
+
+	if(!stash)
+	{
+		stash = ft_strndup("", 1);
+		if (!stash)
+			return (NULL);
+	}
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0)
+	{
+		free(stash);
+		return (NULL);
+	}
+	total_size = ft_strlen(stash);
+	line = NULL;
+	if (bytes_read == 0 && total_size > 0)
+		line = ft_strndup(stash, total_size);
+	while (bytes_read > 0)
+	{
+		stash = ft_strjoin(stash, buffer, total_size, bytes_read);
+		total_size += bytes_read;
+		new_line_index = ft_find_index_in_range(stash, '\n', total_size - bytes_read, total_size);
+		if (new_line_index <= total_size)
+		{
+			line = ft_strndup(stash, new_line_index + 1);
+			if (!line)
+				return (NULL);
+			stash = ft_create_new_stash(stash, new_line_index + 1, total_size);
+			if (!stash)
+				return (NULL);
+			total_size -= new_line_index;
+			return line;
+		}
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(stash);
+			return (NULL);
+		}
+	}
+	if (bytes_read == 0 && !line)
+		free(stash);
+	return (line);
+}
+
+int	main(void)
+{
+	int fd = open("test.txt", O_RDONLY);
+	char c;
+	while ((1))
+	{
+		scanf("%c", &c);
+		char *r = get_next_line(fd);
+		if (r == NULL)
+			break;
+		printf("%s", r);
+		free(r);
+	}
+	
+	return (0);
+}
---
+++ get_next_line3/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kvalerii <kvalerii@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 12:16:09 by kvalerii          #+#    #+#             */
+/*   Updated: 2024/10/31 17:16:19 by kvalerii         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+//! to remove
+# include <fcntl.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+
+#endif
